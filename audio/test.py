7ef04e8ebb0b7f44abe58afe425216de9a6ad533
Saikat Das <sd.saikat396@gmail.com>
2026-02-20 17:55:42 +0530

    removed some useless files

--- audio/test.py
+++
@@ -1,9 +0,0 @@
-from ytmusicapi import YTMusic
-import json
-
-ytmusic = YTMusic()
-
-song = ytmusic.get_watch_playlist("7gBadWs9Bu8")
-
-with open("song.json", "w") as file:
-    json.dump(song, file, indent=4)
